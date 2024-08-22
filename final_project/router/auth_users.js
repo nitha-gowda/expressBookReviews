cff27647b1f88a20d44c05bddbc3bd25a55a0bb0
nitha-gowda <169087231+nitha-gowda@users.noreply.github.com>
2024-08-22 17:44:04 -0400

    task8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -25,8 +25,8 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  const username = req.query.username;
-    const password = req.query.password;
+  const username = req.body.username;
+    const password = req.body.password;
     // Check if username or password is missing
     if (!username || !password) {
         return res.status(404).json({ message: "Error logging in" });
@@ -36,12 +36,12 @@ regd_users.post("/login", (req,res) => {
         // Generate JWT access token
         let accessToken = jwt.sign({
             data: password
-        }, 'access', { expiresIn: 60 * 60 });
+        }, 'access', { expiresIn: 60 * 60 * 60});
         // Store access token and username in session
         req.session.authorization = {
             accessToken, username
         }
-        return res.status(200).send("User successfully logged in");
+        return res.status(200).send({message : "User successfully logged in"});
     } else {
         return res.status(208).json({ message: "Invalid Login. Check username and password" });
     }
@@ -49,8 +49,10 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.session.authorization.username;
+  const review = req.query.review;
+  
+  res.send(username);
 });
 
 module.exports.authenticated = regd_users;
